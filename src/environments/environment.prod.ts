69f228393b5ca737a4bb3819194806eb17812dd5
unknown <strawhorse@gmail.com>
2026-05-07 13:20:30 +0000

    api key added to environment variables - should never do this in reality

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: false,
+  tmdbApiKey: 'dcaa68b8ed74bede1d4dcf7a2e00fc53',
+  // tmdbAccessToken: 'your-new-access-token-here', Not needed here, although this apparently is a newer method
+  tmdbBaseUrl: 'https://api.themoviedb.org/3'
 };
+
+// copied over from the test environment file, although this is replicated when you build the app to run
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  tmdbApiKey: 'dcaa68b8ed74bede1d4dcf7a2e00fc53',
+  // tmdbAccessToken: 'your-new-access-token-here', Not needed here, although this apparently is a newer method
+  tmdbBaseUrl: 'https://api.themoviedb.org/3'
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// It's generally not good to expose any api keys to the public or where someone could decrypt it from an app, but since I'm just building a college app
+// it's fine. In reality, it's better to use a backend somewhere to make the api calls and then serve them to the mobile app instead
